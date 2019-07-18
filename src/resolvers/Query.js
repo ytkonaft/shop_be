81908f4bb8fca9223f97e12d6438f62c587d8571
Artem <boho.ufo@gmail.com>
2019-07-18 17:58:36 +0300

    fetch in upload link is removed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  products: forwardTo("db")
+  products: forwardTo("db"),
+  product: forwardTo("db")
   //   async products(parent, args, ctx, info) {
   //     const products = await ctx.db.query.products();
   //     console.log(products);
