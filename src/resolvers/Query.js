150e6b0468f591da286fea7a75a2635995f0e4fa
Artem <boho.ufo@gmail.com>
2019-07-26 17:21:32 +0300

    pagination in progress

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   products: forwardTo("db"),
-  product: forwardTo("db")
+  product: forwardTo("db"),
+  productsConnection: forwardTo("db")
   //   async products(parent, args, ctx, info) {
   //     const products = await ctx.db.query.products();
   //     console.log(products);
